455b1c52387877303e9c2fe4d5d58df04d17ddd2
Shaker Funkhouser <vonfunkhouserprod@gmail.com>
2019-07-28 23:44:37 +0000

    Automatically backed up by Learn

--- db/schema.rb
+++ db/schema.rb
@@ -10,12 +10,6 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 1) do
-
-  create_table "users", force: :cascade do |t|
-    t.string "username"
-    t.string "password"
-    t.integer "balance"
-  end
+ActiveRecord::Schema.define(version: 0) do
 
 end
